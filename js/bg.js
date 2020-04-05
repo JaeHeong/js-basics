1bd4a29e2af99a4440ed66bd52fe8d179caf4c38
JaeHeong <rlawogud970301@gmail.com>
2020-04-05 12:22:37 +0900

    거의다함
    css빼고 기본틀은 다 만듦

---
+++ js/bg.js
@@ -0,0 +1,45 @@
+/**니꼬꺼 밑에 잇음
+ * Math.floor(Math.random() * 6); 랜덤 숫자 만드는 수학 함수
+ */
+
+const background = document.querySelector("body");
+
+function backGround() {
+  const rndNum = Math.floor(Math.random() * 6);
+  const image = new Image(); /**new Date처럼 new Image는 이미지 하나 const로 만들어준다. */
+  image.src = `images/${rndNum +
+    1}.jpg`; /**img.src 는 이렇게 사용한다. jpg붙여줘야한다.  */
+  image.classList.add("bgStyle");
+  background.appendChild(image);
+  /**이미지는 색깔이 아니니까 이렇게 넣어준다.
+   */
+}
+
+function init() {
+  backGround();
+}
+
+init();
+
+/*const body = document.querySelector("body");
+
+const IMG_NUMBER = 4;
+
+function paintImage(imgNUMBER) {
+  const image = new Image();
+  image.src = `images/${imgNUMBER + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+}
+
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+
+init();*/
---
+++ js/weather.js
@@ -0,0 +1,123 @@
+/**니꼬형꺼 밑에 있음 */
+
+const weather = document.querySelector(".js-weather");
+const API_KEY = "e92e9821cc75ae92a20559f2ecbddcd5";
+const COORDS = "coords";
+
+function saveWeather(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function painWeather(temp, place) {
+  weather.innerText = `Today ${temp}'C at ${place};`; /**${}이걸 잘 이용하자 */
+}
+
+function getWeather(lat, lon) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  ) /**API 사용할 때 이렇게 fetch라는 함수를 사용해준다. 주의할점은 ``백틱을 사용해줘야한다. */
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temp = json.main.temp;
+      const place = json.name;
+      painWeather(temp, place);
+    });
+}
+
+function successCallback(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const coordsObj = {
+    latitude: lat,
+    longitude: lon
+  };
+  getWeather(lat, lon);
+  saveWeather(coordsObj);
+}
+
+function errorCallback() {
+  console.log("Where are you?");
+}
+
+function loadcoords() {
+  const currentCoords = localStorage.getItem(COORDS);
+  if (currentCoords === null) {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    /**현재 위치 가져오는 함수이다! */
+  } else {
+    const parsedCoords = JSON.parse(currentCoords);
+
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadcoords();
+}
+
+init();
+
+/**
+ * const weather = document.querySelector(".js-weather");
+
+const API_KEY = "e92e9821cc75ae92a20559f2ecbddcd5";
+
+const COORDS = "coords";
+
+function getWeather(lat, lon) {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+      const temperature = json.list["0"].main.temp;
+      const place = json.city.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
+
+ */
